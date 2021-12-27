b20e7340badfef3d906716007df6c54daf11d08f
Scott Deeg <sdeeg@planet10.us>
2021-12-27 13:48:05 -0800

    switch http to restful package.  add http and json mime return type.

--- cmd/eight-ball/main.go
+++ cmd/eight-ball/main.go
@@ -2,45 +2,66 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"math/rand"
 	"net/http"
-)
 
-//restful "github.com/emicklei/go-restful/v3"
+	restful "github.com/emicklei/go-restful/v3"
+)
 
 // TODO: map these to Properties
 const (
-	Port          = ":8888"
-	AppName       = "go-eight-ball"
-	DefaultAnswer = "Answer is default"
+	Port    = ":8888"
+	AppName = "go-eight-ball"
 )
 
-//TODO: return html
-func get_default(writer http.ResponseWriter, req *http.Request) {
+var DefaultAnswer = []string{"Default 0", "Default 1", "Default 2", "Default 3"}
 
-	fmt.Fprintf(writer, "<http><body>"+AppName+": <a href>/random-answer</a></body></http>")
+func get_default(req *restful.Request, resp *restful.Response) {
+
+	resp.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(resp, "<http><head></head><body>%s: <a href=\"/random-answer\">random-answer</a></body></http>", AppName)
 }
 
-func random_answer(writer http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprintf(writer, DefaultAnswer)
+func hello(req *restful.Request, resp *restful.Response) {
+	resp.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(resp, "<http><head></head><body>Hello</body></http>")
 }
+
+//restful version
+func random_answer(req *restful.Request, resp *restful.Response) {
+
+	resp.Header().Set("Content-Type", "application/json; charset=utf-8")
+	fmt.Fprintf(resp, "{ \"answer\": \"%s\" }", DefaultAnswer[rand.Intn(4)])
+}
+
+//http version
+// func random_answer(writer http.ResponseWriter, req *http.Request) {
+// 	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+// 	fmt.Fprintf(writer, "{ \"answer\": \"%s\" }", DefaultAnswer)
+// }
 
 func main() {
 
 	// http.HandleFunc("/", get_default)
 	// http.HandleFunc("/random-answer", random_answer)
 
-	// ws := new(restful.WebService)
-	// ws.Route(ws.GET("/hello").To(hello))
-	// restful.Add(ws)
+	//Using restful package
+	ws := new(restful.WebService)
+	ws.Route(ws.GET("/").To(get_default))
+	ws.Route(ws.GET("/hello").To(hello))
+	ws.Route(ws.GET("/random-answer").To(random_answer))
+	restful.Add(ws)
 
-	log.Fatal(http.ListenAndServe(Port, nil))
+	log.Println("Starting server on port " + Port)
+	s := &http.Server{
+		Addr: Port,
+		// Handler:        myHandler,
+		// ReadTimeout:    10 * time.Second,
+		// WriteTimeout:   10 * time.Second,
+		// MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 
-	// fmt.Println("Starting server on port " + Port)
-	// http.ListenAndServe(":"+Port, nil)
 }
-
-// func hello(req *restful.Request, resp *restful.Response) {
-// 	io.WriteString(resp, "world")
-// }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/sdeeg-vmware/go-eight-ball
 
 go 1.17
 
-require github.com/emicklei/go-restful/v3 v3.3.3 // indirect
+require (
+	github.com/emicklei/go-restful/v3 v3.7.3 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+)
